166e89ebdea9d8bdee8a6fd911dd3174ae63dfea
toniahurst <antonia.a.hurst@gmail.com>
2019-08-14 17:20:32 +0000

    L17. Added sectins to courses.

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,15 +1,13 @@
 class Instructor::CoursesController < ApplicationController
-	before_action :authenticate_user!
-	def new
-		@course = Course.new
+  before_action :authenticate_user!
 
-	def create
+  def new
+    @course = Course.new
+  end
+
+  def create
     @course = current_user.courses.create(course_params)
-    if @course.valid?
-      redirect_to instructor_course_path(@course)
-    else
-      render :new, status: :unprocessable_entity
-    end
+    redirect_to instructor_course_path(@course)
   end
 
   def show
@@ -22,4 +20,3 @@ class Instructor::CoursesController < ApplicationController
     params.require(:course).permit(:title, :description, :cost)
   end
 end
-

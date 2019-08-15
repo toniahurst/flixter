31dc9fc9d5c4b29ea86fd2ef6041f9f7c42ea2ae
toniahurst <antonia.a.hurst@gmail.com>
2019-08-15 00:29:52 +0000

    L27. Added student enrollments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
-    	resources :lessons, only: [:new, :create]
+      resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]

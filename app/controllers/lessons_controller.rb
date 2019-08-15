167b79686d4752934c9f40a2a165a683e784a011
toniahurst <antonia.a.hurst@gmail.com>
2019-08-15 01:58:58 +0000

    L30. Added ranking to lessons.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,7 +1,7 @@
 class LessonsController < ApplicationController
 	before_action :authenticate_user!
-	before_action :require_authorized_for_current_course, only: [:show]
 	
+
 def show
 end
   private

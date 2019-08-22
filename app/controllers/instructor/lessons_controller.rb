1fcad8cec94a283f53133313084b30a2d3ba8567
toniahurst <antonia.a.hurst@gmail.com>
2019-08-22 20:55:01 +0000

    L39. Still tweaking. Trying to get heroku working.

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -3,8 +3,8 @@ class Instructor::LessonsController < ApplicationController
   before_action :require_authorized_for_current_section, only: [:create]
   before_action :require_authorized_for_current_lesson, only: [:update]
 
-  def new
-  end
+  # def new
+  # end
   
   def create
     @lesson = current_section.lessons.create(lesson_params)
@@ -18,16 +18,18 @@ class Instructor::LessonsController < ApplicationController
 
   private
 
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
+
+
   def require_authorized_for_current_lesson
     if current_lesson.section.course.user != current_user
       render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
-  end
-
+  
   def require_authorized_for_current_section
     if current_section.course.user != current_user
       render plain: 'Unauthorized', status: :unauthorized
